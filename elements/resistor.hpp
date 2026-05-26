919c895ad8b97b8fdb8f40b376e7b6e84069192b
alicia <arisa240700@gmail.com>
2026-05-27 04:27:30 +0800

    feat: basic model for resistors

---
+++ elements/resistor.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Resistor{
+public:
+    Resistor(std::string n, std::string n1, std::string n2, std::string v):
+                name(n), node1(n1), node2(n2), value(v) {}
+    ~Resistor() = default;
+
+private:
+    std::string name;
+    std::string node1;
+    std::string node2;
+    std::string value;
+};
